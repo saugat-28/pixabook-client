ca7b41799225609f41c9795b986592c9b2fc015a
saugat-28 <55578635+saugat-28@users.noreply.github.com>
2022-12-13 19:24:15 +0530

    Auto Disabled SignIn SignUp Loading state on Error

--- src/state/actions/auth.js
+++ src/state/actions/auth.js
@@ -1,7 +1,7 @@
 import { AUTH } from '../../constants/actionTypes';
 import * as api from '../../api/index';
 
-export const signIn = (formData, navigate) => async (dispatch) => {
+export const signIn = (formData, navigate, setIsLoading) => async (dispatch) => {
     try {
         const { data } = await api.signIn(formData);
 
@@ -9,18 +9,20 @@ export const signIn = (formData, navigate) => async (dispatch) => {
 
         navigate('/');
     } catch (error) {
-        console.log(error);
+        console.log(error.data);
     }
+    setIsLoading(false);
 }
 
-export const signUp = (formData, navigate) => async (dispatch) => {
+export const signUp = (formData, navigate, setIsLoading) => async (dispatch) => {
     try {
         const { data } = await api.signUp(formData);
 
         dispatch({ type: AUTH, data });
-
+        console.log(data);
         navigate('/');
     } catch (error) {
         console.log(error);
     }
+    setIsLoading(false);
 }
